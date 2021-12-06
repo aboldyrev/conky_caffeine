a8b6ebdd03b99d575c90e529822e687ce50e8061
aboldyrev <andreybold@bk.ru>
2021-12-07 00:36:39 +0700

    Добавлено отображение времени

--- script.lua
+++ script.lua
@@ -30,7 +30,34 @@ function conky_main()
     
     cr = cairo_create(cs)
 
+    draw_time(cr)
+
     cairo_destroy(cr)
     cairo_surface_destroy(cs)
     cr = nil
+end
+
+function draw_time(cr)
+    draw_text(cr, 64, colors.white, 786, 568, conky_parse('${time %H:%M}'))
+    draw_text(cr, 32, colors.white, 972, 568, conky_parse('${time %S}'))
+    draw_text(cr, 16, colors.white, 835, 588, conky_parse('${time %a, %e %b %Y}'))
+
+    -- Закомментировать или удалить, если не нужно
+    draw_text(cr, 16, colors.white, 860, 618, conky_parse('${tztime Europe/Moscow %H:%M:%S}'))
+
+    draw_text(cr, 32, colors.white, 1130, 568, conky_parse('$uptime_short'))
+end
+
+function draw_text(cr, font_size, color, x, y, text)
+    red, green, blue, alpha = rgb_to_r_g_b(color, 1)
+    cairo_select_font_face (cr, font_face, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_set_font_size (cr, font_size)
+    cairo_set_source_rgba (cr, red, green, blue, alpha)
+    cairo_move_to (cr, x, y)
+    cairo_show_text (cr, text)
+    cairo_stroke (cr)
+end
+
+function rgb_to_r_g_b(colour,alpha)
+    return ((colour / 0x10000) % 0x100) / 255., ((colour / 0x100) % 0x100) / 255., (colour % 0x100) / 255., alpha
 end
